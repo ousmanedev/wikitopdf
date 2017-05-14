92a45e5166cabdc66b62e5d7a3b45ff332bace4f
Rahmane OUSMANE <ousmanedev@gmail.com>
2017-05-15 06:12:05 +0800

    First working version.

---
+++ wikitopdf.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "" ]; then
+  echo "You must provide a Github repo name in the format owner/repo"
+  exit 1
+fi
+
+git clone "https://github.com/$1.wiki.git" "wiki"
+
+if [[ $? -ne 0 ]]; then
+  exit 1
+fi
+
+cd "wiki"
+
+for file in *.md
+do
+  echo "\chapter{$file}" >> "wiki.tex"
+
+  pandoc "$file" -f "markdown_github" -t "latex" >> "wiki.tex"
+done
+
+cd "../"
+
+pandoc "wiki/wiki.tex" -s -o "wiki.pdf" --variable documentclass="report" --toc
+
+rm -rf "wiki"
+
+echo "PDF generated at wiki.pdf"
+
+exit 0
